fc8eb9fd4d6af97c009867eafbcbff7f84ccf72b
Mathis ROME <mathis.rome@icloud.com>
2025-02-14 10:00:47 +0100

    fix(history): Margin

--- app/src/pages/GameHistoryPage.tsx
+++ app/src/pages/GameHistoryPage.tsx
@@ -18,13 +18,14 @@ export default function GameHistoryPage () {
     return <>
         <h1 className="text-3xl font-bold mb-3 mt-3 text-center uppercase">Historique des parties</h1>
 
-        {games ? games.map((game) => { return <div className="flex w-full">
+        {games ? games.map((game) => { return <div className="flex w-full mb-3">
             <div className="card bg-base-300 rounded-box grid h-32 flex-grow place-items-center">
                 <h2 className={'text-center font-bold text-2xl uppercase'}>Joueur 1</h2>
                 <h3 className={'text-center font-bold text-xl'}>
-                     {game.creator}
+                    {game.creator}
                 </h3>
-                {game.creator === game.winner ? <div className="badge badge-primary">Gagnant</div> : <div className="badge badge-secondary">Perdant</div>}
+                {game.creator === game.winner ? <div className="badge badge-primary">Gagnant</div> :
+                    <div className="badge badge-secondary">Perdant</div>}
 
             </div>
             <div className="divider divider-horizontal">VS</div>
@@ -33,8 +34,10 @@ export default function GameHistoryPage () {
                 <h3 className={'text-center font-bold text-xl'}>
                     {game.player}
                 </h3>
-                {game.player === game.winner ? <div className="badge badge-primary">Gagnant</div> : <div className="badge badge-secondary">Perdant</div>}
+                {game.player === game.winner ? <div className="badge badge-primary">Gagnant</div> :
+                    <div className="badge badge-secondary">Perdant</div>}
             </div>
-        </div>}) : ""}
+        </div>
+        }) : ""}
     </>
 }
